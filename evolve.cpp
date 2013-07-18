6841b54e4c7c391f3524dbaa95aaca3aaece9384
Igor Kozlov <ikozlov@physics.mcgill.ca>
2013-07-18 16:33:13 -0400

    Bugfix attempt to free unallocated arrays.
    rk_flag+1 should have crashed the program (as rk_flag was 1), but apperently didn't do it. Matt's compiler considered function parameter 1+1 as 1 (no crash), but 2 as 2 (crash). Puzzle remained unsolved.

--- evolve.cpp
+++ evolve.cpp
@@ -167,14 +167,14 @@ int Evolve::EvolveIt(InitData *DATA, Grid ***arena, Grid ***Lneighbor, Grid ***R
       for(int ieta=0; ieta<DATA->neta; ieta++)
       {
  	util->cube_free(arena[ix][iy][ieta].TJb,rk_order+1, 5,4);
-	util->cube_free(arena[ix][iy][ieta].dUsup,rk_order+1, 5,4);
+	util->cube_free(arena[ix][iy][ieta].dUsup,rk_order+1, 4,4);
 	util->cube_free(arena[ix][iy][ieta].Wmunu,rk_order+1, 5,4);
 	util->cube_free(arena[ix][iy][ieta].prevWmunu,1, 5,4);
 	util->cube_free(arena[ix][iy][ieta].Pimunu,rk_order+1, 5,4);
 	util->cube_free(arena[ix][iy][ieta].prevPimunu,1, 5,4);
 	util->mtx_free(arena[ix][iy][ieta].u,rk_order+1, 4);
 	util->mtx_free(arena[ix][iy][ieta].a,rk_order+1, 4);
-	util->mtx_free(arena[ix][iy][ieta].prev_u,rk_order+1, 4);
+	util->mtx_free(arena[ix][iy][ieta].prev_u,1, 4);
 	util->vector_free(arena[ix][iy][ieta].theta_u);
 	util->vector_free(arena[ix][iy][ieta].pi_b);
  	
