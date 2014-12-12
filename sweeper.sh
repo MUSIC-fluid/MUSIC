10f42af5bdf692cce60250194d89ad2f7c892abb
Chun Shen <chunshen1987@gmail.com>
2014-12-12 10:35:10 -0500

    forgot one file

--- sweeper.sh
+++ sweeper.sh
@@ -4,5 +4,6 @@ folder_name=$1
 echo Moving all the results into $folder_name ... 
 mkdir $folder_name
 mv *.dat $folder_name
+mv *.gp $folder_name
 cp input $folder_name
 mv $folder_name/known_nuclei.dat ./
