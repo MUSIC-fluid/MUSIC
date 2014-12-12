ca1c40c621e38dd4113c720f35a45ddb451a8e98
Chun Shen <chunshen1987@gmail.com>
2014-12-12 10:34:08 -0500

    add a shell script to sweep all the generated results into a results folder

---
+++ sweeper.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+folder_name=$1
+echo Moving all the results into $folder_name ... 
+mkdir $folder_name
+mv *.dat $folder_name
+cp input $folder_name
+mv $folder_name/known_nuclei.dat ./
