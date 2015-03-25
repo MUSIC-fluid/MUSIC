ec12a2f6a3b55a93d92ddb393b490ede1db2c170
Chun Shen <chunshen1987@gmail.com>
2015-03-25 15:34:27 -0400

    add a new class grid_info to collect all the output functions for the information on the grid

---
+++ src/grid_info.cpp
@@ -0,0 +1,16 @@
+#include "util.h"
+#include "grid_info.h"
+
+using namespace std;
+
+Grid_info::Grid_info()
+{
+
+}
+
+Grid_info::~Grid_info()
+{
+
+}
+
+
---
+++ src/grid_info.h
@@ -0,0 +1,44 @@
+#ifndef GRID_info_H
+#define GRID_inf0_H
+#include "data.h"
+#include "eos.h"
+#include "Grid.h"
+#include <mpi.h>
+#include <iostream>
+#include <iomanip>
+
+class Grid_info
+{
+    private:
+
+    public:
+        Grid_info();
+        ~Grid_info();
+
+        void ComputeV2(InitData *DATA, Grid ***arena, double tau); //added
+        void ComputeEccentricity(InitData *DATA, Grid ***arena, double tau); //added
+        void print_rhob_evolution(InitData *DATA, Grid ***arena, double tau, EOS* eos, int rank);
+        void print_qmu_evolution(InitData *DATA, Grid ***arena, double tau, EOS* eos, int rank);
+        void print_fireball_evolution_on_phasediagram(InitData *DATA, Grid ***arena, double tau, EOS* eos, int rank);
+        void ComputeAnisotropy(InitData *DATA, Grid ***arena, double tau); //added
+        void PrintGrid(Grid *grid_p, int rk_order);
+        void LinkNeighbors(InitData *DATA, Grid ****arena);
+        void InitTJb(InitData *DATA, Grid ****arena);
+        void PrintAxy(InitData *DATA, Grid ***arena, double tau);
+        void PrintAxy2(InitData *DATA, Grid ***arena, double tau);
+        void PrintdEdEta(InitData *DATA, Grid ***arena);
+        void OutputEvolutionDataXYZ(Grid ***arena, InitData *DATA, EOS *eos, double tau, int size, int rank);
+        void OutputEvolutionDataXYEta(Grid ***arena, InitData *DATA, EOS *eos, double tau, int size, int rank);
+        void OutputEvolutionDataXYEta_finite_muB(Grid ***arena, InitData *DATA, EOS *eos, double tau, int size, int rank);
+        void OutputPlotDataXYZ(Grid ***arena, InitData *DATA, EOS *eos, double tau, int size, int rank);
+        void OutputEvolutionOSCAR(Grid ***arena, InitData *DATA, EOS *eos, double tau, int size, int rank);
+        void OutputXY(Grid ***arena, InitData *DATA, EOS *eos, double tau, int size, int rank);
+        void PrintArena(Grid ***arena, InitData *DATA, double tau);
+        void PrintEtaEpsilon(Grid ***arena, InitData *DATA, double tau, int size, int rank);
+        void PrintxEpsilon(Grid ***arena, InitData *DATA, double tau, int size, int rank);
+        void ComputeEnergyConservation(InitData *DATA, Grid ***arena, double tau);
+        void getAverageTandPlasmaEvolution(Grid ***arena, InitData *DATA, EOS *eos, double tau, int size, int rank);
+        void Output_hydro_information_header(InitData *DATA, EOS *eos);
+        void Tmax_profile(Grid ***arena, InitData *DATA, EOS *eos, double tau, int size, int rank);
+};
+#endif
