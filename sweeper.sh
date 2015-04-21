4830650eb689e64df7b5c91efca9188fd4216649
Chun Shen <chunshen1987@gmail.com>
2015-04-21 08:52:08 -0400

    sweep hydro_header into results folder after the hydro run

--- sweeper.sh
+++ sweeper.sh
@@ -15,6 +15,7 @@ mkdir $folder_name
 mv *.dat $folder_name
 mv *.err $folder_name
 mv *.log $folder_name
+mv hydro_info_header_h $folder_name
 cp music_input_2 $folder_name/music_input
 mv $folder_name/known_nuclei.dat ./
 rm -fr $folder_name/tmp*.dat
