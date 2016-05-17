be2638409b5cbbc4871fcebc75950b56711dffc7
Chun Shen <chunshen1987@gmail.com>
2016-05-17 11:10:15 -0400

    some clean up in the u_derivative

--- src/u_derivative.h
+++ src/u_derivative.h
@@ -1,5 +1,5 @@
-#ifndef U_DERIVATIVE_H
-#define U_DERIVATIVE_H
+#ifndef U_DERIVATIVE_H_
+#define U_DERIVATIVE_H_
 
 #include "util.h"
 #include "data.h"
@@ -7,24 +7,23 @@
 #include <string.h>
 #include <iostream>
 
-class U_derivative{
-
+class U_derivative {
  private:
-  Minmod *minmod;
-  // Sangyong Nov 18 2014: added EOS *eos;
-  EOS *eos;
+     Minmod *minmod;
+     // Sangyong Nov 18 2014: added EOS *eos;
+     EOS *eos;
   
  public:
-  // Sangyong Nov 18 2014: added EOS *eos in the argument
-  U_derivative(EOS *eosIn, InitData* DATA_in);//constructor
-  ~U_derivative();
-  int UpdateDSpatial(double tau, InitData *DATA, Grid *grid_pt, Grid *Lneighbor, Grid *Rneighbor, 
-		     Grid *Lneighbor2, Grid *Rneighbor2, int rk_flag, int size, int rank);
-  int MakedU(double tau, InitData *DATA, Grid ***arena, Grid ***Lneighbor, Grid ***Rneighbor, int rk_flag, int size, int rank);
-  int MakeDSpatial(double tau, InitData *DATA, Grid *grid_pt, Grid *Lneighbor, Grid *Rneighbor, 
-		   Grid *Lneighbor2, Grid *Rneighbor2, int rk_flag, int size, int rank);
-  int MakeDTau(double tau, InitData *DATA, Grid *grid_pt, int rk_flag);
-  int UpdateDTau(double tau, InitData *DATA, Grid *grid_pt, int rk_flag);
-  int UpdatedU(double tau, InitData *DATA, Grid ***arena, Grid ***Lneighbor, Grid ***Rneighbor, int rk_flag, int size, int rank);
+     // Sangyong Nov 18 2014: added EOS *eos in the argument
+     U_derivative(EOS *eosIn, InitData* DATA_in);  // constructor
+     ~U_derivative();
+     int MakedU(double tau, InitData *DATA, Grid ***arena,
+                Grid ***Lneighbor, Grid ***Rneighbor, int rk_flag,
+                int size, int rank);
+     int MakeDSpatial(double tau, InitData *DATA, Grid *grid_pt,
+                      Grid *Lneighbor, Grid *Rneighbor, 
+		              Grid *Lneighbor2, Grid *Rneighbor2,
+                      int rk_flag, int size, int rank);
+     int MakeDTau(double tau, InitData *DATA, Grid *grid_pt, int rk_flag);
 };
 #endif
