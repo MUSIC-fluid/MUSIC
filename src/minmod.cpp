fac06fd9e5f0e7fb4dfc1f5a30b37081ef9fb221
Chun Shen <chunshen1987@gmail.com>
2016-05-18 15:33:21 -0400

    some clean up in minmod.cpp

--- src/minmod.cpp
+++ src/minmod.cpp
@@ -1,55 +1,45 @@
 #include "util.h"
 #include "minmod.h"
 
-Minmod::Minmod(InitData *DATA)
-{
+Minmod::Minmod(InitData *DATA) {
     theta_flux = DATA->minmod_theta;
 }
 
 // destructor
-Minmod::~Minmod()
-{
+Minmod::~Minmod() {
 }
 
-double Minmod::minmod_dx(double up1, double u, double um1)
-{
-  double diffup = (up1 - u)*theta_flux;
-  double diffdown = (u - um1)*theta_flux;
-  double diffmid = (up1 - um1)*0.5;
+double Minmod::minmod_dx(double up1, double u, double um1) {
+    double diffup = (up1 - u)*theta_flux;
+    double diffdown = (u - um1)*theta_flux;
+    double diffmid = (up1 - um1)*0.5;
 
-  double tempf;
-  if( (diffup > 0.0) && (diffdown > 0.0) && (diffmid > 0.0) )
-  {
-    tempf = mini(diffdown, diffmid);
-    return mini(diffup, tempf);
-  }
-  else if( (diffup < 0.0) && (diffdown < 0.0) && (diffmid < 0.0) )
-  {
-    tempf = maxi(diffdown, diffmid);
-    return maxi(diffup, tempf);
-  }
-  else 
-    return 0.0;
+    double tempf;
+    if ( (diffup > 0.0) && (diffdown > 0.0) && (diffmid > 0.0) ) {
+        tempf = mini(diffdown, diffmid);
+        return mini(diffup, tempf);
+    } else if ( (diffup < 0.0) && (diffdown < 0.0) && (diffmid < 0.0) ) {
+        tempf = maxi(diffdown, diffmid);
+        return maxi(diffup, tempf);
+    } else {
+      return 0.0;
+    }
 }/* minmod_dx */
 
 
-double Minmod::minmod_theta_dx(double up1, double u, double um1, double theta)
-{
-  double diffup = (up1 - u)*theta;
-  double diffdown = (u - um1)*theta;
-  double diffmid = (up1 - um1)*0.5;
+double Minmod::minmod_theta_dx(double up1, double u, double um1, double theta) {
+    double diffup = (up1 - u)*theta;
+    double diffdown = (u - um1)*theta;
+    double diffmid = (up1 - um1)*0.5;
 
-  double tempf;
-  if( (diffup > 0.0) && (diffdown > 0.0) && (diffmid > 0.0) )
-  {
-    tempf = mini(diffdown, diffmid);
-    return mini(diffup, tempf);
-  }
-  else if( (diffup < 0.0) && (diffdown < 0.0) && (diffmid < 0.0) )
-  {
-    tempf = maxi(diffdown, diffmid);
-    return maxi(diffup, tempf);
-  }
-  else
-    return 0.0;
+    double tempf;
+    if ( (diffup > 0.0) && (diffdown > 0.0) && (diffmid > 0.0) ) {
+        tempf = mini(diffdown, diffmid);
+        return mini(diffup, tempf);
+    } else if ( (diffup < 0.0) && (diffdown < 0.0) && (diffmid < 0.0) ) {
+        tempf = maxi(diffdown, diffmid);
+        return maxi(diffup, tempf);
+    } else {
+        return 0.0;
+    }
 }/* minmod_dx */
--- src/minmod.h
+++ src/minmod.h
@@ -1,17 +1,18 @@
-#ifndef MINMOD_H
-#define MINMOD_H
+// Copyright 2011 @ Bjoern Schenke, Sangyong Jeon, and Charles Gale
+#ifndef SRC_MINMOD_H_
+#define SRC_MINMOD_H_
 
 #include "data.h"
 
-class Minmod
-{
-    private:
-        double theta_flux;
+class Minmod {
+ private:
+    double theta_flux;
 
-    public:
-        Minmod(InitData* DATA);
-        ~Minmod();
-        double minmod_dx(double up1, double u, double um1);
-        double minmod_theta_dx(double up1, double u, double um1, double theta);
+ public:
+    Minmod(InitData* DATA);
+    ~Minmod();
+    double minmod_dx(double up1, double u, double um1);
+    double minmod_theta_dx(double up1, double u, double um1, double theta);
 };
-#endif
+
+#endif  // SRC_MINMOD_H_
