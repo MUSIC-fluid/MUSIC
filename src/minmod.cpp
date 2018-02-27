d70875de81c8ce7a3912c46092b03c0ed2f080df
Richard Barnes <rbarnes@umn.edu>
2018-02-27 17:21:54 -0500

    Move minmod into minmod.h

--- src/minmod.cpp
+++ src/minmod.cpp
@@ -4,17 +4,3 @@
 Minmod::Minmod(InitData *DATA) {
     theta_flux = DATA->minmod_theta;
 }
-
-double Minmod::minmod_dx(double up1, double u, double um1) {
-    double diffup   = (up1 - u)*theta_flux;
-    double diffdown = (u - um1)*theta_flux;
-    double diffmid  = (up1 - um1)*0.5;
-
-    if ( (diffup > 0.0) && (diffdown > 0.0) && (diffmid > 0.0) ) {
-        return std::min(std::min(diffdown, diffmid), diffup);
-    } else if ( (diffup < 0.0) && (diffdown < 0.0) && (diffmid < 0.0) ) {
-        return std::max(std::max(diffdown, diffmid), diffup);
-    } else {
-      return 0.0;
-    }
-}/* minmod_dx */
--- src/minmod.h
+++ src/minmod.h
@@ -10,7 +10,20 @@ class Minmod {
 
  public:
     Minmod(InitData* DATA);
-    double minmod_dx(double up1, double u, double um1);
+
+    double minmod_dx(double up1, double u, double um1) {
+        double diffup   = (up1 - u)*theta_flux;
+        double diffdown = (u - um1)*theta_flux;
+        double diffmid  = (up1 - um1)*0.5;
+
+        if ( (diffup > 0.0) && (diffdown > 0.0) && (diffmid > 0.0) ) {
+            return std::min(std::min(diffdown, diffmid), diffup);
+        } else if ( (diffup < 0.0) && (diffdown < 0.0) && (diffmid < 0.0) ) {
+            return std::max(std::max(diffdown, diffmid), diffup);
+        } else {
+          return 0.0;
+        }
+    }/* minmod_dx */
 };
 
 #endif  // SRC_MINMOD_H_
