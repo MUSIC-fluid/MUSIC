4f39c85860eeb35f0834210df67dfbc60d120d0f
Chun Shen <chunshen1987@gmail.com>
2018-07-04 14:23:24 -0400

    change eos class

--- src/eos.h
+++ src/eos.h
@@ -1,86 +1,36 @@
-// Copyright 2011 @ Bjoern Schenke, Sangyong Jeon, and Charles Gale
+// Copyright 2018 @ Chun Shen
+
 #ifndef SRC_EOS_H_
 #define SRC_EOS_H_
 
-#include <iostream>
-#include <vector>
-#include <string>
+#include "eos_idealgas.h"
+#include "eos_hotQCD.h"
 
-#include "util.h"
-#include "data.h"
-#include "pretty_ostream.h"
 
+//! This is a wrapper class for the equation of state
 class EOS {
  private:
-    const InitData& parameters_ptr;
-    pretty_ostream music_message;
+    double (EOS::*pressure_ptr)(double e, double rhob);
 
-    int number_of_tables;
-    std::vector<double> nb_bounds;
-    std::vector<double> e_bounds;
-
-    std::vector<double> nb_spacing;
-    std::vector<double> e_spacing;
-
-    std::vector<int> nb_length;
-    std::vector<int> e_length;
-
-    double ***pressure_tb;
-    double ***temperature_tb;
-    double ***mu_B_tb;
-    double ***mu_S_tb;
-
-    int whichEOS;
-    double eps_max;
+    EOS_idealgas eos_ideal;
+    EOS_hotQCD eos_HQCD;
 
  public:
-    EOS() = default;
-    EOS(const InitData &para_in);  // constructor
-    ~EOS();  // destructor
-    void initialize_eos();
-    std::string get_hydro_env_path() const;
-    void resize_table_info_arrays();
-    void init_eos();                 // for whichEOS=1
+    EOS() {
+        pressure_ptr = &EOS::get_pressure_idealgas;
+    }
 
-    void init_eos_s95p(int selector);
-    void init_eos9();
-    void init_eos10();   // for EOS at finite mu_B from A. M.
-    void init_eos11();   // foe EoS at finite mu_B from Pasi
-    void init_eos12();   // for EOS at finite mu_B from A. M.
+    ~EOS() {};
 
-    // returns maximum local energy density of the EoS table
-    // in the unit of [1/fm^4]
-    double get_eps_max() const { return(eps_max); }
+    double get_pressure_idealgas(double e, double rhob) {
+        return(eos_ideal.get_pressure(e, rhob));
+    }
 
-    // for EOS at finite mu_B
-    int get_table_idx(double e) const;
-    double interpolate1D(double e, int table_idx, double ***table) const;
-    double interpolate2D(double e, double rhob, int table_idx, double ***table) const;
+    double get_pressure(double e, double rhob) {
+        return((this->*(pressure_ptr))(e, rhob));
+    }
 
-    double get_cs2(double e, double rhob) const;
-    double calculate_velocity_of_sound_sq(double e, double rhob) const;
-    double get_rhob_from_mub   (double e, double mub) const;
-    double get_dpOverde_WB     (double e) const;
-    double get_dpOverde3       (double e, double rhob) const;
-    double get_dpOverdrhob2    (double e, double rhob) const;
-    double p_rho_func          (double e, double rhob) const;
-    double p_e_func            (double e, double rhob) const;
-    double T_from_eps_ideal_gas(double eps) const;
-    double get_entropy         (double epsilon, double rhob) const;
-    double get_temperature     (double epsilon, double rhob) const;
-    double get_temperature_WB  (double e_local) const;
-    double get_mu              (double epsilon, double rhob) const;
-    double get_muS             (double epsilon, double rhob) const;
-    double get_pressure        (double epsilon, double rhob) const;
-    double get_pressure_WB     (double e_local) const;
-    double s2e_ideal_gas(double s) const;
-    double get_s2e(double s, double rhob) const;
-    double get_s2e_finite_rhob(double s, double rhob) const;
-    void check_eos() const;
-    void check_eos_with_finite_muB() const;
-    void check_eos_no_muB() const;
-    void output_eos_matrix(int ne, int nrhob, double** matrix_ptr,
-                           std::string filename) const;
+
 };
 
 #endif  // SRC_EOS_H_
--- src/eos_unittest.cpp
+++ src/eos_unittest.cpp
@@ -1,12 +1,12 @@
 // Copyright 2018 @ Chun Shen
 
-#include "eos_wrapper.h"
+#include "eos.h"
 #include "doctest.h"
 
 #include <cassert>
 #include <iostream>
 
 TEST_CASE("test constructor") {
-    EOS_wrapper test;
+    EOS test;
     CHECK(test.get_pressure(1.0, 0.0) == 1./3.);
 }
