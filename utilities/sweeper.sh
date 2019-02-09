9a1417c557ce13face77affb009a461c3c5f08c5
Chun Shen <chunshen1987@gmail.com>
2019-02-09 11:15:07 -0500

    small changes

--- utilities/sweeper.sh
+++ utilities/sweeper.sh
@@ -15,8 +15,4 @@ mkdir $folder_name
 mv *.dat $folder_name
 mv *.err $folder_name
 mv *.log $folder_name
-mv hydro_info_header_h $folder_name
-cp music_input_2 $folder_name/music_input
-mv $folder_name/known_nuclei.dat ./
-mv $folder_name/eps_freeze_list_*.dat ./
-rm -fr $folder_name/tmp*.dat
+cp music_input_mode_2 $folder_name/music_input
