0a29ff0e779bbaf75e5ec9da73bc7c5fbd54ebb6
Chun Shen <chunshen1987@gmail.com>
2019-07-04 10:59:54 -0400

    allow overwrite the MUSIC running mode

--- src/read_in_parameters.cpp
+++ src/read_in_parameters.cpp
@@ -803,6 +803,9 @@ InitData read_in_parameters(std::string input_file) {
 
 void set_parameter(InitData &parameter_list, std::string parameter_name,
                    double value) {
+    if (parameter_name == "MUSIC_mode") {
+        parameter_list.mode = static_cast<int>(value);
+    }
     if (parameter_name == "output_evolution_data") {
         parameter_list.outputEvolutionData = static_cast<int>(value);
     }
