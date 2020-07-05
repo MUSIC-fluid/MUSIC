de900dab6cdda270126f34b48a7860e3aaca3c45
Chun Shen <chunshen1987@gmail.com>
2020-07-05 16:02:10 -0400

    accept reset tau_0

--- src/read_in_parameters.cpp
+++ src/read_in_parameters.cpp
@@ -915,6 +915,9 @@ void set_parameter(InitData &parameter_list, std::string parameter_name,
     if (parameter_name == "MUSIC_mode")
         parameter_list.mode = static_cast<int>(value);
 
+    if (parameter_name == "Initial_time_tau_0")
+        parameter_list.tau0 = value;
+
     if (parameter_name == "output_evolution_data")
         parameter_list.outputEvolutionData = static_cast<int>(value);
 
