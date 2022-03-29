c5aa256e8efd6509535777ad19d9c682f80f1ec1
Willian Matioli Serenone <willian.matioli@gmail.com>
2022-03-29 11:01:34 -0300

    Update number of tables of EOS-BEST

--- src/eos_best.cpp
+++ src/eos_best.cpp
@@ -40,11 +40,20 @@ void EOS_BEST::initialize_eos() {
     music_message << "from path " << path;
     music_message.flush("info");
 
-    const int ntables = 6;
+    const int ntables = 87;
     set_number_of_tables(ntables);
     resize_table_info_arrays();
 
-    string eos_file_string_array[6] = {"0", "1", "2", "3", "4", "5"};
+    string eos_file_string_array[ntables] = {"0",  "1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9",
+                                            "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+                                            "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+                                            "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+                                            "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+                                            "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+                                            "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+                                            "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+                                            "80", "81", "82", "83", "84", "85", "86"};
+
     pressure_tb    = new double** [ntables];
     temperature_tb = new double** [ntables];
     mu_B_tb        = new double** [ntables];
@@ -102,7 +111,7 @@ void EOS_BEST::initialize_eos() {
         }
     }
 
-    double eps_max_in = e_bounds[5] + e_spacing[5]*e_length[5];
+    double eps_max_in = e_bounds[ntables-1] + e_spacing[ntables-1]*e_length[ntables-1];
     set_eps_max(eps_max_in);
 
     music_message.info("Done reading EOS.");
