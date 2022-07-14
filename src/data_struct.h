0531abf1fa23d20ef9d342eecfcbe1d201c214e5
Chun Shen <chunshen1987@gmail.com>
2022-07-14 10:42:30 -0400

    do not use assumed_align because of potential compiling error from apple m1

--- src/data_struct.h
+++ src/data_struct.h
@@ -36,25 +36,25 @@ typedef struct {
     float ux, uy, ueta;
 } fluidCell_ideal;
 
-template<typename T>
-T assume_aligned(T x) {
-  #if defined(__AVX512__)
-    constexpr int i = 64;
-  #elif defined(__AVX__)
-    constexpr int i = 32;
-  #elif defined(__SSE2__)
-    constexpr int i = 16;
-  #else
-  #error please set alignment i 
-  #endif
-  #ifdef __ICC
-    T r = x;
-    __assume_aligned(r,i);
-    return r;
-  #else
-    return reinterpret_cast<T>(__builtin_assume_aligned(x,i));
-  #endif
-}
+//template<typename T>
+//T assume_aligned(T x) {
+//  #if defined(__AVX512__)
+//    constexpr int i = 64;
+//  #elif defined(__AVX__)
+//    constexpr int i = 32;
+//  #elif defined(__SSE2__)
+//    constexpr int i = 16;
+//  #else
+//  #error please set alignment i 
+//  #endif
+//  #ifdef __ICC
+//    T r = x;
+//    __assume_aligned(r,i);
+//    return r;
+//  #else
+//    return reinterpret_cast<T>(__builtin_assume_aligned(x,i));
+//  #endif
+//}
 
 
 #endif  // _SRC_DATA_STRUCT_H_
