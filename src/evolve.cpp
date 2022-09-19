76024c92b403264aec98b0f768e9788536721b53
Sahr Abdullah Alzhrani <gj7671@warrior.nas.grid.wayne.edu>
2022-09-19 10:44:50 -0400

    output additional information for 111 and 112

--- src/evolve.cpp
+++ src/evolve.cpp
@@ -172,7 +172,8 @@ int Evolve::EvolveIt(SCGrid &arena_prev, SCGrid &arena_current,
         }
         grid_info.output_momentum_anisotropy_vs_tau(
                                             tau, -0.5, 0.5, *ap_current);
-        if (DATA.Initial_profile == 13) {
+        if (DATA.Initial_profile == 13
+            || DATA.Initial_profile == 111 || DATA.Initial_profile == 112) {
             grid_info.output_average_phase_diagram_trajectory(
                                             tau, -0.5, 0.5, *ap_current);
             grid_info.output_average_phase_diagram_trajectory(
