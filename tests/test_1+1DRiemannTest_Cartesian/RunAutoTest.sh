1dd547fe4ea9667f713ee2a1c143a126990cb858
Chun Shen <chunshen1987@gmail.com>
2023-01-23 20:06:07 -0500

    update the automated tests

--- tests/test_1+1DRiemannTest_Cartesian/RunAutoTest.sh
+++ tests/test_1+1DRiemannTest_Cartesian/RunAutoTest.sh
@@ -38,3 +38,42 @@ else
     echo "Tests FAILED :("
     exit 1
 fi
+
+./MUSIChydro tests/test_1+1DRiemannTest_Cartesian/music_input_1+1DRiemannTest_z_viscous
+
+python3 tests/test_1+1DRiemannTest_Cartesian/TestOutputVisFiles.py
+STATUS=$?
+
+if [ $STATUS == 0 ]; then
+    echo "All tests passed! :)"
+    rm -fr ./*.dat
+else
+    echo "Tests FAILED :("
+    exit 1
+fi
+
+./MUSIChydro tests/test_1+1DRiemannTest_Cartesian/music_input_1+1DRiemannTest_y_viscous
+
+python3 tests/test_1+1DRiemannTest_Cartesian/TestOutputVisFiles.py
+STATUS=$?
+
+if [ $STATUS == 0 ]; then
+    echo "All tests passed! :)"
+    rm -fr ./*.dat
+else
+    echo "Tests FAILED :("
+    exit 1
+fi
+
+./MUSIChydro tests/test_1+1DRiemannTest_Cartesian/music_input_1+1DRiemannTest_x_viscous
+
+python3 tests/test_1+1DRiemannTest_Cartesian/TestOutputVisFiles.py
+STATUS=$?
+
+if [ $STATUS == 0 ]; then
+    echo "All tests passed! :)"
+    rm -fr ./*.dat
+else
+    echo "Tests FAILED :("
+    exit 1
+fi
