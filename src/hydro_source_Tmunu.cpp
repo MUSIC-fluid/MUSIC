eb4b3a8ae52183f888f39d49f0c5b681bd9e9ba9
Anar Akbarov <anarakberov1@gmail.com>
2024-09-17 14:16:48 -0400

    Fixed errors in hydro_source_Tmunu.cpp

--- src/hydro_source_Tmunu.cpp
+++ src/hydro_source_Tmunu.cpp
@@ -7,6 +7,7 @@
 #include <cstdlib>
 #include <vector>
 #include <array>
+#include <omp.h>  // Include for OpenMP
 
 using Util::hbarc;
 
@@ -41,33 +42,103 @@ void HydroSourceTmunu::readIPGevent() {
             DATA.ny, std::vector<std::array<double, 10>>(DATA.neta)));
 
     std::string line;
+    double tau0 = DATA.tau0;
+    std::ifstream profile(DATA.initName.c_str());  // Fixed the ifstream
+
+    std::string dummy;
+    // read the information line
+    std::getline(profile, dummy);
+
+    const int nx = DATA.nx;  // Changed to correct variable from DATA
+    const int ny = DATA.ny;  // Changed to correct variable from DATA
+    std::vector<double> temp_profile_ed(nx * ny, 0.0);
+    std::vector<double> temp_profile_utau(nx * ny, 0.0);
+    std::vector<double> temp_profile_ux(nx * ny, 0.0);
+    std::vector<double> temp_profile_uy(nx * ny, 0.0);
+    std::vector<double> temp_profile_ueta(nx * ny, 0.0);
+    std::vector<double> temp_profile_pitautau(nx * ny, 0.0);
+    std::vector<double> temp_profile_pitaux(nx * ny, 0.0);
+    std::vector<double> temp_profile_pitauy(nx * ny, 0.0);
+    std::vector<double> temp_profile_pitaueta(nx * ny, 0.0);
+    std::vector<double> temp_profile_pixx(nx * ny, 0.0);
+    std::vector<double> temp_profile_pixy(nx * ny, 0.0);
+    std::vector<double> temp_profile_pixeta(nx * ny, 0.0);
+    std::vector<double> temp_profile_piyy(nx * ny, 0.0);
+    std::vector<double> temp_profile_piyeta(nx * ny, 0.0);
+    std::vector<double> temp_profile_pietaeta(nx * ny, 0.0);
+
     // Step2: Reading IP-Glasma data
     int ix, iy, ieta;
-    double pitautau, pitaux, pitauy, pitaueta;
-    double pixx*DATA.sFactor, pixy*DATA.sFactor, pixeta*DATA.sFactor, piyy*DATA.sFactor, piyeta*DATA.sFactor, pietaeta*DATA.sFactor;
-    double e*DATA.sFactor/hbarc, utau, ux, uy, ueta;
-    double tau = DATA.tau0;
 
-    while (std::getline(IPGinputFile, line)) {
-        std::istringstream iss(line);
+    double e, utau, ux, uy, ueta;
+    double density, dummy1, dummy2, dummy3;
+    double pixx, pixy, pixeta, piyy, piyeta, pietaeta, pitautau, pitaux, pitauy, pitaueta;
 
-        iss >> ix >> iy >> ieta >> e >> utau >> ux >> uy >> ueta
-            >> pitautau >> pitaux >> pitauy >> pitaueta
-            >> pixx*DATA.sFactor >> pixy >> pixeta >> piyy >> piyeta >> pietaeta;
+    for (ix = 0; ix < nx; ix++) {
+        for (iy = 0; iy < ny; iy++) {
+            int idx = iy + ix * ny;
+            std::getline(profile, dummy);
+            std::stringstream ss(dummy);  // Fixed the issue with ss and iss
+            ss >> ix >> iy >> ieta >> e >> utau >> ux >> uy >> ueta
+               >> pitautau >> pitaux >> pitauy >> pitaueta
+               >> pixx >> pixy >> pixeta >> piyy >> piyeta >> pietaeta;
 
+            ueta = ueta * tau0;
+            temp_profile_ed[idx] = density * DATA.sFactor / hbarc;  // 1/fm^4
+            temp_profile_ux[idx] = ux;
+            temp_profile_uy[idx] = uy;
+            temp_profile_ueta[idx] = ueta;
+            temp_profile_utau[idx] = sqrt(1. + ux * ux + uy * uy + ueta * ueta);
 
-       ueta = ueta * tau;
+            // Viscous components already in 1/fm^4 from the IP-Glasma output
+            double visFactor = DATA.sFactor * DATA.preEqVisFactor;
+            temp_profile_pixx[idx] = pixx * visFactor;
+            temp_profile_pixy[idx] = pixy * visFactor;
+            temp_profile_pixeta[idx] = pixeta * tau0 * visFactor;
+            temp_profile_piyy[idx] = piyy * visFactor;
+            temp_profile_piyeta[idx] = piyeta * tau0 * visFactor;
 
-        
-        pitaueta *= tau;
-        pietaeta *= tau * tau*Data.sFactor;
-        pixeta *= tau*Data.sFactor;
-        piyeta *= tau*Data.sFactor;
+            utau = temp_profile_utau[idx];
+            temp_profile_pietaeta[idx] = (
+                (2. * (ux * uy * temp_profile_pixy[idx]
+                       + ux * ueta * temp_profile_pixeta[idx]
+                       + uy * ueta * temp_profile_piyeta[idx])
+                 - (utau * utau - ux * ux) * temp_profile_pixx[idx]
+                 - (utau * utau - uy * uy) * temp_profile_piyy[idx])
+                / (utau * utau - ueta * ueta));
+            temp_profile_pitaux[idx] = (1. / utau
+                                        * (temp_profile_pixx[idx] * ux
+                                           + temp_profile_pixy[idx] * uy
+                                           + temp_profile_pixeta[idx] * ueta));
+            temp_profile_pitauy[idx] = (1. / utau
+                                        * (temp_profile_pixy[idx] * ux
+                                           + temp_profile_piyy[idx] * uy
+                                           + temp_profile_piyeta[idx] * ueta));
+            temp_profile_pitaueta[idx] = (1. / utau
+                                          * (temp_profile_pixeta[idx] * ux
+                                             + temp_profile_piyeta[idx] * uy
+                                             + temp_profile_pietaeta[idx] * ueta));
+            temp_profile_pitautau[idx] = (1. / utau
+                                          * (temp_profile_pitaux[idx] * ux
+                                             + temp_profile_pitauy[idx] * uy
+                                             + temp_profile_pitaueta[idx] * ueta));
 
-        energy_density_[ix][iy][ieta] = e;
-        velocity_[ix][iy][ieta] = {utau, ux, uy, ueta};
-        shear_tensor_[ix][iy][ieta] = {pitautau, pitaux, pitauy, pitaueta*tau*Data.sFactor,
-                                       pixx*DATA.sFactor, pixy*DATA.sFactor, pixeta*DATA.sFactor, piyy*DATA.sFactor, piyeta*DATA.sFactor, pietaeta}; //multiply Data.sFactor
+            if (ix == 0 && iy == 0) {
+                DATA.x_size = -dummy2 * 2;
+                DATA.y_size = -dummy3 * 2;
+                if (omp_get_thread_num() == 0) {
+                    music_message << "eta_size=" << DATA.eta_size
+                                  << ", x_size=" << DATA.x_size
+                                  << ", y_size=" << DATA.y_size;
+                    music_message.flush("info");
+                }
+            }
+        }
+
+        // Store the data in the containers
+        double tau = DATA.tau0;  // Fixed tau declaration
+        shear_tensor_[ix][iy][ieta] = {pitautau, pitaux, pitauy, pitaueta * tau * DATA.sFactor,
+                                       pixx * DATA.sFactor, pixy * DATA.sFactor, pixeta * DATA.sFactor, piyy * DATA.sFactor, piyeta * DATA.sFactor, pietaeta}; // multiply by DATA.sFactor
     }
 
     IPGinputFile.close();
