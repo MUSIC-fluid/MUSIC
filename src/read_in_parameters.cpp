aed972ab20a2e04ddfaabd810e8969b748e1f65f
Anar Akbarov <anarakberov1@gmail.com>
2026-01-12 17:19:01 -0500

    Add bulk viscosity from chemical non-equilibrium feature
    - Add FlagBulkChem parameter to enable/disable chemical bulk viscosity
    - Add chem_rate_C parameter for chemical reaction rate constant
    - Automatically set turn_on_bulk_chem based on FlagBulkChem flag
    - Update example input file with new parameters

--- src/read_in_parameters.cpp
+++ src/read_in_parameters.cpp
@@ -1,4 +1,3 @@
-
 #include "read_in_parameters.h"
 
 #include <cstring>
@@ -391,6 +390,20 @@ InitData read_in_parameters(std::string input_file) {
     // net baryon diffusion: kappa coefficient
     parameter_list.kappa_coefficient =
         (getParameter(input_file, "kappa_coefficient", 0.0));
+
+    // chemical reaction rate for bulk viscosity from chemical non-equilibrium
+    parameter_list.chem_rate_C =
+        (getParameter(input_file, "chem_rate_C", 1.0));
+
+    // Set turn_on_bulk_chem automatically based on FlagBulkChem
+    if (parameter_list.FlagBulkChem) {
+        parameter_list.turn_on_bulk_chem = 1;
+        music_message << "turn_on_bulk_chem is automatically set to 1 "
+                      << "because FlagBulkChem = 1";
+        music_message.flush("info");
+    } else {
+        parameter_list.turn_on_bulk_chem = 0;
+    }
 
     parameter_list.store_hydro_info_in_memory =
         (getParameter(input_file, "store_hydro_info_in_memory", 0));
